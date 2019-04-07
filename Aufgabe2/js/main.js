8be5ab0bc733f5e4d83537e52a788f59084581e6
KatharinaSchmitt <44029017+KatharinaSchmitt@users.noreply.github.com>
2019-04-07 13:22:35 +0200

    Mau Mau - ziehen und Karten generieren

---
+++ Aufgabe2/js/main.js
@@ -0,0 +1,68 @@
+/*
+Aufgabe: <Aufgabe 2 Mau Mau>
+Name: <Katharina Schmitt>
+Matrikel: <260918>
+Datum: <07.04.2019>
+    
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
+let anzahl;
+function eingabeAnzahlHandkarten() {
+    anzahl = prompt("Anzahl Spielkarten");
+}
+eingabeAnzahlHandkarten();
+let wert = ["7", "8", "9", "10", "Bube", "Dame", "König", "As"];
+let symbol;
+let alleKarten = [];
+function erstelleKartenarray() {
+    for (let i = 0; i <= 32; i++) {
+        if (i < 8) {
+            symbol = "Herz " + wert[i];
+            alleKarten.splice(0, 0, "Herz " + wert[i]);
+        }
+        else if (i >= 8 && i < 16) {
+            symbol = "Pik " + wert[i % 8];
+            alleKarten.splice(0, 0, "Pik " + wert[i % 8]);
+        }
+        else if (i >= 16 && i < 24) {
+            symbol = "Karo " + wert[i % 8];
+            alleKarten.splice(0, 0, "Karo " + wert[i % 8]);
+        }
+        else if (i >= 24 && i < 32) {
+            symbol = "Kreuz " + wert[i % 8];
+            alleKarten.splice(0, 0, "Kreuz " + wert[i % 8]);
+        }
+    }
+}
+erstelleKartenarray();
+function verteileKarten() {
+    let handkarten = [];
+    for (let y = parseFloat(anzahl); handkarten.length <= y - 1;) {
+        let x = 0;
+        ;
+        {
+            x = Math.floor((Math.random() * alleKarten.length));
+            x = x - 1;
+            handkarten.push(alleKarten[x]);
+            let prodElement = document.createElement('div');
+            let karte = `
+            <p class="${alleKarten[x]}">${alleKarten[x]}</p>
+            `;
+            prodElement.innerHTML = karte;
+            document.getElementById("Handkarten").appendChild(prodElement);
+            alleKarten.splice(x, 1);
+        }
+    }
+}
+verteileKarten();
+function zeigeKartenstapel() {
+    for (let b = 0; b <= alleKarten.length - 1; b++) {
+        let prodElement = document.createElement('div');
+        let karte = `
+            <p class="${alleKarten[b]}">${alleKarten[b]}</p>
+            `;
+        prodElement.innerHTML = karte;
+        document.getElementById("Spielkarten").appendChild(prodElement);
+    }
+}
+zeigeKartenstapel();
+//# sourceMappingURL=main.js.map
---
+++ Aufgabe2/js/main.ts
@@ -0,0 +1,81 @@
+/*
+Aufgabe: <Aufgabe 2 Mau Mau>
+Name: <Katharina Schmitt>
+Matrikel: <260918>
+Datum: <07.04.2019>
+	
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
+
+let anzahl: string;
+
+function eingabeAnzahlHandkarten () : void {
+     anzahl = prompt ("Anzahl Spielkarten");
+}
+
+eingabeAnzahlHandkarten ()
+
+let wert: string[] = ["7", "8", "9", "10", "Bube", "Dame", "König", "As"];
+let symbol: string;
+let alleKarten: string [] = [];
+
+function erstelleKartenarray () {
+
+    for (let i:number = 0; i<=32; i++) {
+        if (i<8) { 
+            symbol = "Herz " + wert[i];
+            alleKarten.splice(0, 0, "Herz " + wert[i]);
+            
+        }
+        else if (i>=8 && i < 16) { 
+            symbol = "Pik " + wert[i%8];
+            alleKarten.splice(0, 0, "Pik " + wert[i%8]);
+         }
+        else if (i>=16 && i < 24) {
+            symbol = "Karo " + wert[i%8];
+            alleKarten.splice(0, 0, "Karo " + wert[i%8]);
+            
+        }
+        else if (i>=24 && i<32) {
+            symbol = "Kreuz " + wert[i%8];
+            alleKarten.splice(0, 0, "Kreuz " + wert[i%8]);
+        }
+    }       
+}
+erstelleKartenarray ()
+
+function verteileKarten () {
+    let handkarten:string [] = [];
+       
+    for (let y = parseFloat (anzahl); handkarten.length <= y-1;) {
+        let x:number = 0; ; {
+            x = Math.floor((Math.random() * alleKarten.length));
+            x= x - 1; 
+
+            handkarten.push(alleKarten[x]);
+            
+            let prodElement = document.createElement('div');
+            let karte: string= `
+            <p class="${alleKarten[x]}">${alleKarten[x]}</p>
+            `
+            prodElement.innerHTML = karte;
+            document.getElementById("Handkarten").appendChild(prodElement);
+            
+            alleKarten.splice(x, 1);
+        }
+    }
+}
+
+verteileKarten ()
+
+function zeigeKartenstapel () {
+    for ( let b : number = 0; b <= alleKarten.length - 1 ; b++) {
+        let prodElement = document.createElement('div');
+            let karte: string= `
+            <p class="${alleKarten[b]}">${alleKarten[b]}</p>
+            `
+            prodElement.innerHTML = karte;
+            document.getElementById("Spielkarten").appendChild(prodElement);
+    }
+}
+
+zeigeKartenstapel ()
